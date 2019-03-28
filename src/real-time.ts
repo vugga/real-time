466b0714ee830a045763e34f2e5cab7ffca94cc7
Ceddy Muhoza <ceddymuhoza@gmail.com>
2019-03-28 17:02:30 -0400

    exports

--- src/real-time.ts
+++ src/real-time.ts
@@ -1,2 +1,5 @@
-export * from './redis-presence';
-export * from './socket-server';
+import redisPresence from './redis-presence';
+import socketServer from './socket-server';
+export const SocketServer = socketServer;
+export const RedisPresence = redisPresence;
+export default SocketServer;
